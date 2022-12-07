660305913b850becfb09cde84b87924e6ec9da3d
Pieroggo <greg2mazur@gmail.com>
2022-12-07 18:36:07 +0100

    day 2 finished

--- 2015/Day2.py
+++ 2015/Day2.py
@@ -5,4 +5,18 @@ totalWrapping=0
 totalRibbon=0
 for line in lines:
     measurements=line.split('x')
-    print()
+    a=int(measurements[0])
+    b = int(measurements[1])
+    c = int(measurements[2])
+    totalWrapping+=((2*a*b)+(2*a*c)+(2*b*c))
+    totalRibbon+=(a*b*c)
+    if(a==min(a,b,c)):
+        totalWrapping +=(a*min(b,c))
+        totalRibbon+=2*(a+min(b,c))
+    elif(b == min(a, b, c)):
+        totalWrapping += (b * min(a, c))
+        totalRibbon += 2 * (b + min(a, c))
+    else:
+        totalWrapping += (c * min(b, a))
+        totalRibbon += 2 * (c + min(b, a))
+print(f"Wrapping: {totalWrapping} Ribbon: {totalRibbon}")
